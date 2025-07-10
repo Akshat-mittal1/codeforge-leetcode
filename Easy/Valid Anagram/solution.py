0aa1ebffc3c103b75fd4dd0a481a776986210d39
Akshat Mittal <akshatmittal798@gmail.com>
2025-07-10 20:07:29 +0530

    Create solution.py

---
+++ Easy/Valid Anagram/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        a = sorted(s)
+        b = sorted(t)
+        if a != b:
+            return False
+        return True
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.isAnagram("anagram", "nagaram"))  # True
+    print(sol.isAnagram("rat", "car"))          # False
